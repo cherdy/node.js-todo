2a7c51ca49e6d445fd50d44b8402f4b97bd5ac47
cherdyy <maxim123456789101112@gmail.com>
2020-02-02 21:28:19 +0200

    а

---
+++ controllers/todo.js
@@ -0,0 +1,6 @@
+module.exports = (router) => {
+
+    const routes = router();
+
+
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+    app = express(),
+    inject = require('require-all'),
+    router = express.Router,
+    bodyParser = require('body-parser');
+
+const port = 3000,
+    host = '127.0.0.1';
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+try {
+    const controllers = inject(__dirname + '/controllers');
+
+    for (const name in controllers) {
+        app.use(`/${name}`, controllers[name](router))
+    }
+} catch (e) {
+    console.log(e)
+}
+
+app.listen(port, () => {
+    console.log('app listening')
+});
